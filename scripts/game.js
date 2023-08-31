fb26a349d868ba3a3319712742b72702608cd069
Mihaly Lovrencsics <lomysa@gmail.com>
2023-08-31 22:15:39 +0000

    Add first passing test

---
+++ scripts/game.js
@@ -0,0 +1,14 @@
+//Javascript structure
+//game {object}     store the game state
+//newGame()         Reset everything and start a new turn
+//addTurn()         Add a random move to the sequence
+//showTurns()       Play the sequence and uses jvascript interval to call the lightsUp function which will light up the appropiate circle
+//playerTurn()      will check i the player clicked the right circle in the sequence
+//showScore()       update the score in the DOM
+
+let game = {
+    score : 0,
+};
+
+
+module.exports = { game };
